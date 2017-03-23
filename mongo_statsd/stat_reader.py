91eced640283a6f32262658f85fca6b0cf137fb0
dantezhu <dantezhu@qq.com>
2017-03-23 10:34:26 +0800

    读取结束

--- mongo_statsd/stat_reader.py
+++ mongo_statsd/stat_reader.py
@@ -4,6 +4,7 @@
 """
 
 import os
+import re
 
 
 class StatReader(object):
@@ -23,11 +24,28 @@ class StatReader(object):
         :return:
         """
 
-        result = dict()
+        """
+        connected to: 127.0.0.1
+        insert  query update delete getmore command flushes mapped  vsize    res non-mapped faults            locked db idx miss %     qr|qw   ar|aw  netIn netOut  conn       time
+        132     *0     *0     *0       0   184|0       0  1181g  2368g  7.38g      1187g      2 texas_statistic:0.8%          0       0|0     0|0    53k    23k  1060   10:17:56
+
+        """
+
+        keys = list()
+        values = list()
 
         for line in os.popen(self.cmd):
-            values = line.split(':')
-            if len(values) == 2:
-                result[values[0].strip()] = int(values[1].strip())
+            if line.startswith('connect'):
+                continue
+            if line.startswith('insert'):
+                # 所有的key
+                keys = re.split(r'\s+', line)
+                continue
+
+            # 最后这个一定是values
+            values = re.split(r'\s+', line)
+            break
+
+        result = dict(zip(keys, values))
 
         return result
